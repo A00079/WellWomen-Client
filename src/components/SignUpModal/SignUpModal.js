40b68c0d6114ce90c4c8dd955ca9b0add8d949f8
Ajay 007 <ajaybendre@credenceanalytics.com>
2020-09-09 13:46:44 +0530

    Fix in signup modal

--- src/components/SignUpModal/SignUpModal.js
+++ src/components/SignUpModal/SignUpModal.js
@@ -123,6 +123,11 @@ const SignUpModal = (props) => {
                            </div>
                                 <Link to="/signup">
                                     <button
+                                    style={{    
+                                        width: '213px',
+                                        margin: '0 auto',
+                                        marginTop: '1rem',
+                                        marginBttom: '1rem'}}
                                         className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
                                     >
                                         <svg
