c33bb1c5ef8b3133009ecdbd54c4201f63da0209
Ajay 007 <ajaybendre@credenceanalytics.com>
2020-09-17 00:16:37 +0530

    removed http

--- src/components/SignUpModal/SignUpModal.js
+++ src/components/SignUpModal/SignUpModal.js
@@ -95,7 +95,7 @@ const SignUpModal = (props) => {
                                         fontSize: '12px',
                                         borderRadius: '4px'
                                     }}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="26" viewBox="10 0 45 45" version="1">
+                                        <svg xmlns="" width="35" height="26" viewBox="10 0 45 45" version="1">
                                             <path fill="#FFFFFF" d="M32 30a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h28a2 2 0 0 1 2 2v28z" />
                                             <path fill="#4267b2" d="M22 32V20h4l1-5h-5v-2c0-2 1.002-3 3-3h2V5h-4c-3.675 0-6 2.881-6 7v3h-4v5h4v12h5z" />
                                         </svg>
