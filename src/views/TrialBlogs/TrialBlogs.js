bccf9ef71e25485b7c4651b1beaa16f1d7a19907
A00079 <bajay0007@gmail.com>
2020-10-05 21:48:14 +0530

    UI Fix

--- src/views/TrialBlogs/TrialBlogs.js
+++ src/views/TrialBlogs/TrialBlogs.js
@@ -2,11 +2,23 @@ import React, { useEffect, useState } from 'react';
 import SignUpModal from '../../components/SignUpModal/SignUpModal.js';
 import Header from "../../components/Header/Header.js";
 import HeaderLinks from "../../components/Header/HeaderLinks.js";
+import Blogs from '../../REST/Blogs.js';
+import { Link } from "react-router-dom";
 
 const TrialBlog = () => {
 
     const [showModal, setShowModal] = useState(false)
+    const [useBlogsOne, setBlogOne] = useState([]);
+    const [useBlogsTwo, setBlogTwo] = useState([]);
+
     useEffect(() => {
+        let api_url = "api/admin/getBlog/read";
+        Blogs
+            .getBlogList(api_url)
+            .then((res) => {
+                setBlogOne(res.data[0])
+                setBlogTwo(res.data[1])
+            })
         const timer = setTimeout(() => {
             setShowModal(true);
         }, 5000);
@@ -18,58 +30,62 @@ const TrialBlog = () => {
                 showModal ? <SignUpModal showModal={showModal} /> : ""
             }
             <div className="mb-32">
-            <Header
-                brand="Well Woman"
-                rightLinks={<HeaderLinks />}
-                fixed
-                color="darkpink"
-                changeColorOnScroll={{
-                    height: 400,
-                    color: "white"
-                }}
-            />
+                <Header
+                    brand="Well Woman"
+                    rightLinks={<HeaderLinks />}
+                    fixed
+                    color="darkpink"
+                    changeColorOnScroll={{
+                        height: 400,
+                        color: "white"
+                    }}
+                />
             </div>
-            <div class="max-w-6xl px-10 my-4 md:ml-8 py-6 bg-white rounded-lg shadow-md">
-                <div class="flex justify-between items-center">
-                    <span class="font-light text-gray-600">mar 10, 2019</span>
-                    <a class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500" href="#">Design</a>
-                </div>
-                <div class="mt-2">
-                    <a class="text-2xl text-gray-700 font-bold hover:text-gray-600" href="#">Accessibility tools for designers and developers</a>
-                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
-                </div>
-                <div class="flex justify-between items-center mt-4">
-                    <a class="text-blue-600 hover:underline" href="#">Read more</a>
-                    <div>
-                        <a class="flex items-center" href="#">
-                            <img class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block" src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=373&q=80" alt="avatar" />
-                            <h1 class="text-gray-700 font-bold">Khatab wedaa</h1>
-                        </a>
+            <section class="text-gray-700 body-font">
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="flex flex-wrap -mx-4 -mb-10 text-center">
+                        {
+
+                            <div class="sm:w-1/2 mb-10 px-4">
+                                <div class="rounded-lg h-64 overflow-hidden">
+                                    <img alt="content" class="object-cover object-center h-full w-full" src={useBlogsOne.imageurl} />
+                                </div>
+                                <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3" style={{ fontFamily: 'Paytone One' }}>{useBlogsOne.title}</h2>
+                                <p style={{ width: '461px', fontFamily: 'Open Sans', whiteSpace: 'break-spaces', overflow: 'hidden', textOverflow: 'ellipsis' }} class="leading-relaxed text-base">{useBlogsOne.shortdiscription}</p>
+                                <button style={{ backgroundColor: '#D00F7F', fontFamily: 'Open Sans', color: '#FFF' }} class="flex mx-auto mt-6 text-white  border-0 py-2 px-5  rounded">
+                                    <Link to={{
+                                        pathname: "/blog/" + useBlogsOne._id,
+                                        state: useBlogsOne
+                                    }}>
+                                        View Blog
+                                                </Link>
+                                </button>
+                            </div>
+                        }
+                        {
+
+                            <div class="sm:w-1/2 mb-10 px-4">
+                                <div class="rounded-lg h-64 overflow-hidden">
+                                    <img alt="content" class="object-cover object-center h-full w-full" src={useBlogsTwo.imageurl} />
+                                </div>
+                                <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3" style={{ fontFamily: 'Paytone One' }}>{useBlogsTwo.title}</h2>
+                                <p style={{ width: '461px', fontFamily: 'Open Sans', whiteSpace: 'break-spaces', overflow: 'hidden', textOverflow: 'ellipsis' }} class="leading-relaxed text-base">{useBlogsTwo.shortdiscription}</p>
+                                <button style={{ backgroundColor: '#D00F7F', fontFamily: 'Open Sans', color: '#FFF' }} class="flex mx-auto mt-6 text-white  border-0 py-2 px-5  rounded">
+                                    <Link to={{
+                                        pathname: "/blog/" + useBlogsTwo._id,
+                                        state: useBlogsTwo
+                                    }}>
+                                        View Blog
+                                            </Link>
+                                </button>
+                            </div>
+
+                        }
                     </div>
                 </div>
-            </div>
+            </section>
 
-            <div class="max-w-6xl px-10 my-4 md:ml-8 py-6 bg-white rounded-lg shadow-md">
-                <div class="flex justify-between items-center">
-                    <span class="font-light text-gray-600">mar 10, 2019</span>
-                    <a class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500" href="#">Design</a>
-                </div>
-                <div class="mt-2">
-                    <a class="text-2xl text-gray-700 font-bold hover:text-gray-600" href="#">Accessibility tools for designers and developers</a>
-                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
-                </div>
-                <div class="flex justify-between items-center mt-4">
-                    <a class="text-blue-600 hover:underline" href="#">Read more</a>
-                    <div>
-                        <a class="flex items-center" href="#">
-                            <img class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block" src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=373&q=80" alt="avatar" />
-                            <h1 class="text-gray-700 font-bold">Khatab wedaa</h1>
-                        </a>
-                    </div>
-                </div>
-            </div>
 
-            
         </React.Fragment>
     );
 }
