f48dfe14eaae3a67f0f0f13f07149ebae2bd7613
A00079 <bajay0007@gmail.com>
2020-10-12 23:46:47 +0530

    UI Fix

--- src/components/SocialContacts/SocialContacts.js
+++ src/components/SocialContacts/SocialContacts.js
@@ -15,12 +15,12 @@ const SocialContacts = () => {
                             </h3>
                         <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Contact us using any of the below social networks you use .</p>
                     </div>
-                    <div class="flex flex-wrap -m-2">
+                    <div class="flex flex-wrap ml-32">
                         <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
                             <div class="h-full flex items-center p-4 rounded-lg">
                                 <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={FB} />
                                 <div class="flex-grow">
-                                    <h2 class="text-gray-900 title-font font-medium">FACEBOOK</h2>
+                                    {/* <h2 class="text-gray-900 title-font font-medium">FACEBOOK</h2> */}
                                 </div>
                             </div>
                         </div>
@@ -28,7 +28,7 @@ const SocialContacts = () => {
                             <div class="h-full flex items-center p-4 rounded-lg">
                                 <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={TW} />
                                 <div class="flex-grow">
-                                    <h2 class="text-gray-900 title-font font-medium">TWITTER</h2>
+                                    {/* <h2 class="text-gray-900 title-font font-medium">TWITTER</h2> */}
                                 </div>
                             </div>
                         </div>
@@ -36,7 +36,7 @@ const SocialContacts = () => {
                             <div class="h-full flex items-center p-4 rounded-lg">
                                 <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={IG} />
                                 <div class="flex-grow">
-                                    <h2 class="text-gray-900 title-font font-medium">INSTAGRAM</h2>
+                                    {/* <h2 class="text-gray-900 title-font font-medium">INSTAGRAM</h2> */}
                                 </div>
                             </div>
                         </div>
