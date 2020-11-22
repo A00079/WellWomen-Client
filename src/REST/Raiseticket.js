f3a8de9a1f4cf7db2964f5c6f2ebceb6829df610
A00079 <bajay0007@gmail.com>
2020-11-23 03:18:49 +0530

    bug fix

--- src/REST/Raiseticket.js
+++ src/REST/Raiseticket.js
@@ -1,5 +1,5 @@
-// const baseUrl = process.env.REACT_APP_API_URL;
-const baseUrl = 'http://localhost:5000/'
+const baseUrl = process.env.REACT_APP_API_URL;
+// const baseUrl = 'http://localhost:5000/'
 const raiseTicket = {
   
   postUserTicket(urls,data) {
